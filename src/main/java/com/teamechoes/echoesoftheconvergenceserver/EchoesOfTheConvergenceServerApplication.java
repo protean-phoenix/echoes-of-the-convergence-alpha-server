4f802ce4f8d71ddacc70b98b4991feced0c97c37
ace-zhang18 <acezhang18@gmail.com>
2023-03-31 01:35:29 -0400

    initial commit

---
+++ src/main/java/com/teamechoes/echoesoftheconvergenceserver/EchoesOfTheConvergenceServerApplication.java
@@ -0,0 +1,13 @@
+package com.teamechoes.echoesoftheconvergenceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoesOfTheConvergenceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoesOfTheConvergenceServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamechoes/echoesoftheconvergenceserver/EchoesOfTheConvergenceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamechoes.echoesoftheconvergenceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoesOfTheConvergenceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
