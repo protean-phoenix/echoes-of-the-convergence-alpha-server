49f478e33c37116232917c8ef090ecac48cad257
ace-zhang18 <acezhang18@gmail.com>
2023-05-08 12:33:18 -0400

    removed AWS key leak

---
+++ eotc-core/src/main/java/TestClient.java
@@ -0,0 +1,10 @@
+import com.teamechoes.echoesoftheconvergence.interfaces.DynamoDBContainer;
+
+public class TestClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DynamoDBContainer.getClient();
+	}
+
+}
--- eotc-core/src/main/java/com/teamechoes/echoesoftheconvergence/interfaces/DynamoDBContainer.java
+++ eotc-core/src/main/java/com/teamechoes/echoesoftheconvergence/interfaces/DynamoDBContainer.java
@@ -1,5 +1,10 @@
 package com.teamechoes.echoesoftheconvergence.interfaces;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
@@ -14,12 +19,30 @@ public class DynamoDBContainer {
 	static AmazonDynamoDB client;
 	static DynamoDB dynamo;
 	static DynamoDBMapper mapper;
-    private static String awsAccessKey = "AKIAROAGYUCSIB2YJKH6";
-    private static String awsSecretKey = "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";
+    private static String awsAccessKey = null;
+    private static String awsSecretKey = null;
 
 
 	private static void initDynamoDB() {
-		
+	    FileReader reader = null;
+		try {
+			reader = new FileReader("aws.properties");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
+	      
+	    Properties p = new Properties();  
+	    try {
+			p.load(reader);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
+	      
+	    awsAccessKey = p.getProperty("awsAccessKey");  
+	    awsSecretKey = p.getProperty("awsSecretKey");  
+	    
 		//Local Testing
 		client = AmazonDynamoDBClientBuilder.standard().withEndpointConfiguration(
 				new AwsClientBuilder.EndpointConfiguration("http://localhost:8000", "us-east-1"))
